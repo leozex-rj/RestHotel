1d4e967dd61a2695668211f54d542d263b9fec3e
Leandro Lopez <leonightwish@gmail.com>
2021-02-16 16:57:44 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "http://localhost:8080/v1/hotels/1?checkin=01/01/2018&checkout=02/01/2018&numAdultos=2&numCriancas=1"
+    }).then(function(data) {
+       $('.greeting-id').append(data.id);
+       $('.greeting-nameCity').append(data.nameCity);
+    });
+});
